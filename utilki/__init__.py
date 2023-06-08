c663dce22980bcb641d15d6d6c2a8485ff6122ca
Khaidar Bikmaev <khaidar@bikmaev.com>
2023-06-08 18:09:26 +0400

    fuck forgor

--- utilki/__init__.py
+++ utilki/__init__.py
@@ -3,4 +3,10 @@
 __version__ = "0.1.0"
 
 from .task_mixin import TaskMixin
-from .log_utils import progress, set_default_logger_name, set_use_print, log
+from .log_utils import (
+    progress,
+    set_default_logger_name,
+    set_use_print,
+    set_callback,
+    log,
+)
